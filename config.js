1070941f6d439c9d2ca5d0fe86648df74f0e14f0
hussainasir27 <sanakhan244288@gmail.com>
2025-02-23 17:08:45 +0500

    Update config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ global.APIs = {
 global.APIKeys = {
     'https://api.xteam.xyz': 'd90a9e986e18778b',
     'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
-    'https://api.neoxr.my.id': 'yourkey',
+    'https://api.neoxr.my.id': '9OVRDZLL#euD0ZSe9Zb_nn3bYG6-H227yYNuX-DZiDZ2Jne5mWQ8',
     'https://violetics.pw': 'beta',
     'https://zenzapis.xyz': 'yourkey',
     'https://api-fgmods.ddns.net': 'fg-dylux'
